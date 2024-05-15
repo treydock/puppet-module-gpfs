f14e1965baac3a8d2c33ecf7bee767234ca87214
Trey Dockendorf <tdockendorf@osc.edu>
2024-05-15 09:41:14 -0400

    Add gpfs_audit type and provider

---
+++ lib/puppet/provider/gpfs_audit/shell.rb
@@ -0,0 +1,133 @@
+# frozen_string_literal: true
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'gpfs'))
+require 'etc'
+require 'uri'
+
+Puppet::Type.type(:gpfs_audit).provide(:shell, parent: Puppet::Provider::Gpfs) do
+  desc ''
+
+  mk_resource_methods
+
+  defaultfor osfamily: :redhat
+
+  commands mmaudit: '/usr/lpp/mmfs/bin/mmaudit'
+
+  def self.instances
+    audits = []
+    mmaudit_output = mmaudit('all', 'list', '-Y')
+    mmaudit_output.each_line do |line|
+      audit = {}
+      l = line.strip
+      next if l.nil?
+      next if l =~ %r{disabled for all devices}
+
+      l = l.split(':')
+      next if l[2] == 'HEADER'
+      next if l.size < 14
+
+      audit[:ensure] = :present
+      audit[:compliant] = if l[6] == 'noncompliant'
+                            :false
+                          else
+                            :true
+                          end
+      audit[:filesystem] = l[7]
+      audit[:name] = audit[:filesystem]
+      audit[:log_fileset] = l[10]
+      audit[:retention] = l[11].to_i
+      all_events = 'ACCESS_DENIED,ACLCHANGE,CLOSE,CREATE,GPFSATTRCHANGE,OPEN,RENAME,RMDIR,UNLINK,XATTRCHANGE'
+      audit[:events] = if l[13] == all_events
+                         ['ALL']
+                       else
+                         l[13].split(',')
+                       end
+      if l[15] == 'FILESET'
+        audit[:filesets] = l[16].split(',')
+      end
+      if l[15] == 'SKIPFILESET'
+        audit[:skip_filesets] = l[16].split(',')
+      end
+      audits << new(audit)
+    end
+    audits
+  end
+
+  def self.prefetch(resources)
+    audits = instances
+    resources.each_key do |name|
+      provider = audits.find do |audit|
+        audit.filesystem == resources[name][:filesystem]
+      end
+      next unless provider
+
+      resources[name].provider = provider
+    end
+  end
+
+  def create
+    mmaudit_args = [
+      resource[:filesystem], 'enable',
+      '--log-fileset', resource[:log_fileset],
+      '--retention', resource[:retention].to_s,
+      '--events', resource[:events].join(',')
+    ]
+    if resource[:compliant].to_sym == :true
+      mmaudit_args << '--compliant'
+    end
+    if !resource[:filesets].nil? && !resource[:filesets].empty?
+      mmaudit_args << '--filesets'
+      mmaudit_args << resource[:filesets].join(',')
+    end
+    if !resource[:skip_filesets].nil? && !resource[:skip_filesets].empty?
+      mmaudit_args << '--skip-filesets'
+      mmaudit_args << resource[:skip_filesets].join(',')
+    end
+    mmaudit(mmaudit_args)
+    @property_hash[:ensure] = :present
+  end
+
+  def destroy
+    mmaudit([resource[:filesystem], 'disable'])
+
+    @property_hash.clear
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def initialize(value = {})
+    super(value)
+    @property_flush = {}
+  end
+
+  type_properties.each do |prop|
+    define_method "#{prop}=".to_sym do |value|
+      @property_flush[prop] = value
+    end
+  end
+
+  def flush
+    if @property_flush[:events]
+      mmaudit([resource[:filesystem], 'update', '--events', @property_flush[:events].join(',')])
+    end
+    if @property_flush[:filesets]
+      enable_filesets = @property_flush[:filesets].sort - @property_hash[:filesets].sort
+      unless enable_filesets.empty?
+        mmaudit([resource[:filesystem], 'update', '--enable-filesets', enable_filesets.join(',')])
+      end
+      disable_filesets = @property_hash[:filesets] - @property_flush[:filesets]
+      unless disable_filesets.empty?
+        mmaudit([resource[:filesystem], 'update', '--disable-filesets', disable_filesets.join(',')])
+      end
+    end
+    if @property_flush[:skip_filesets]
+      disable_filesets = @property_flush[:skip_filesets].sort - @property_hash[:skip_filesets].sort
+      mmaudit([resource[:filesystem], 'update', '--disable-filesets', disable_filesets.join(',')])
+    end
+    # Collect the resources again once they've been changed (that way `puppet
+    # resource` will show the correct values after changes have been made).
+    @property_hash = resource.to_hash
+  end
+end
---
+++ spec/unit/puppet/provider/gpfs_audit/shell_spec.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Puppet::Type.type(:gpfs_audit).provider(:shell) do
+  let(:type) do
+    Puppet::Type.type(:gpfs_audit)
+  end
+  let(:resource) do
+    type.new(name: 'test', provider: 'shell')
+  end
+
+  let(:mmaudits) do
+    [
+      { ensure: :present, name: 'ess', filesystem: 'ess', compliant: :true,
+        log_fileset: '.audit_log', retention: 365, events: ['ALL'] },
+      { ensure: :present, name: 'scratch', filesystem: 'scratch', compliant: :false,
+        log_fileset: 'audit', retention: 25,
+        events: ['ALL'], filesets: ['fastest1', 'fastest2'] },
+      { ensure: :present, name: 'test', filesystem: 'test', compliant: :false,
+        log_fileset: 'audit', retention: 25,
+        events: ['ACCESS_DENIED', 'ACLCHANGE', 'CLOSE', 'CREATE', 'GPFSATTRCHANGE', 'OPEN'],
+        skip_filesets: ['faltest2'] }
+    ]
+  end
+
+  describe 'self.instances' do
+    it 'creates instances' do
+      allow(described_class).to receive(:mmaudit).with('all', 'list', '-Y').and_return(my_fixture_read('mmaudit.out'))
+      expect(described_class.instances.length).to eq(3)
+      expect(described_class.instances[0].instance_variable_get('@property_hash')).to eq(mmaudits[0])
+      expect(described_class.instances[1].instance_variable_get('@property_hash')).to eq(mmaudits[1])
+      expect(described_class.instances[2].instance_variable_get('@property_hash')).to eq(mmaudits[2])
+    end
+
+    it 'creates no instance when no mmaudits returned' do
+      allow(described_class).to receive(:mmaudit).with('all', 'list', '-Y').and_return(my_fixture_read('mmaudit.out').split("\n")[0])
+      expect(described_class.instances.length).to be_zero
+    end
+  end
+
+  describe 'self.prefetch' do
+    let(:instances) do
+      mmaudits.map { |f| described_class.new(f) }
+    end
+    let(:resources) do
+      mmaudits.each_with_object({}) do |f, h|
+        h[f[:name]] = type.new(f.reject { |_k, v| v.nil? })
+      end
+    end
+
+    before(:each) do
+      allow(described_class).to receive(:instances).and_return(instances)
+    end
+
+    it 'prefetches' do
+      resources.each_key do |r|
+        expect(resources[r]).to receive(:provider=).with(described_class)
+      end
+      described_class.prefetch(resources)
+    end
+  end
+
+  describe 'create' do
+    it 'enables mmaudit' do
+      expected = [
+        'test', 'enable',
+        '--log-fileset', '.audit_log',
+        '--retention', '365',
+        '--events', 'ALL'
+      ]
+      expect(resource.provider).to receive(:mmaudit).with(expected)
+      resource.provider.create
+      property_hash = resource.provider.instance_variable_get('@property_hash')
+      expect(property_hash[:ensure]).to eq(:present)
+    end
+  end
+
+  describe 'destroy' do
+    it 'disables mmaudit' do
+      expect(resource.provider).to receive(:mmaudit).with(['test', 'disable'])
+      resource.provider.destroy
+      property_hash = resource.provider.instance_variable_get('@property_hash')
+      expect(property_hash).to eq({})
+    end
+  end
+
+  describe 'flush' do
+    it 'modifies mmaudit' do
+      expect(resource.provider).to receive(:mmaudit).with(['test', 'update', '--events', 'foo,bar'])
+      resource.provider.events = ['foo', 'bar']
+      resource.provider.flush
+    end
+
+    it 'adds filesets' do
+      hash = resource.to_hash
+      hash[:filesets] = ['foo']
+      resource.provider.instance_variable_set(:@property_hash, hash)
+      expect(resource.provider).to receive(:mmaudit).with(['test', 'update', '--enable-filesets', 'bar,baz'])
+      resource.provider.filesets = ['foo', 'bar', 'baz']
+      resource.provider.flush
+    end
+
+    it 'remove filesets' do
+      hash = resource.to_hash
+      hash[:filesets] = ['foo', 'bar', 'baz']
+      resource.provider.instance_variable_set(:@property_hash, hash)
+      expect(resource.provider).to receive(:mmaudit).with(['test', 'update', '--disable-filesets', 'baz'])
+      resource.provider.filesets = ['foo', 'bar']
+      resource.provider.flush
+    end
+  end
+end
